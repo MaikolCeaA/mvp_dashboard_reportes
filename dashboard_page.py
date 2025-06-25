c363ee3eeadefec55a5a0ef4dfcec7b733da976d
MaikolCeaA <maykolcea2@gmail.com>
2025-06-25 11:08:12 -0400

    Add 2 task in the same cell

--- dashboard_page.py
+++ dashboard_page.py
@@ -5,12 +5,12 @@ from datetime import datetime, timedelta
 
 # Reportes simulados
 reportes = {
-    "2025-06-23": {"hora": "11:00", "titulo": "Reunión Clientes", "estado": "Enviado"},
-    "2025-06-24": {"hora": "09:00", "titulo": "Reporte Diario SB", "estado": "Aceptado"},
-    "2025-06-25": {"hora": "10:00", "titulo": "Control Legal", "estado": "Pendiente"},
-    "2025-06-26": {"hora": "11:00", "titulo": "Reunión Clientes", "estado": "Aceptado"},
-    "2025-06-27": {"hora": "11:00", "titulo": "Reunión Clientes", "estado": "Aceptado"},
-    "2025-06-28": {"hora": "15:00", "titulo": "Reporte Semanal", "estado": "Pendiente"}
+    "2025-06-23-Reunión Clientes": {"hora": "01:00", "fecha": "2025-06-23", "titulo": "Reunión Clientes", "estado": "Enviado"}, 
+    "2025-06-24-Reporte Diario SB": {"hora": "01:00", "fecha": "2025-06-23", "titulo": "Reporte Diario SB", "estado": "Aceptado"}, 
+    "2025-06-25-Control Legal": {"hora": "03:00", "fecha": "2025-06-25", "titulo": "Control Legal", "estado": "Pendiente"}, 
+    "2025-06-26-Reunión Clientes": {"hora": "04:00","fecha": "2025-06-26",  "titulo": "Reunión Clientes", "estado": "Aceptado"}, 
+    "2025-06-27-Reunión Clientes": {"hora": "03:00","fecha": "2025-06-27",  "titulo": "Reunión Clientes", "estado": "Aceptado"}, 
+    "2025-06-28-Reporte Semanal": {"hora": "01:00", "fecha": "2025-06-28", "titulo": "Reporte Semanal", "estado": "Pendiente"} 
 }
 # Determinar el color según el estado de la tarea
 status_colors = {
@@ -18,7 +18,8 @@ status_colors = {
     "Pendiente": "#E6007E",
     "Aceptado": "#003865",
 }
-                    
+font_SUL = "Segoe UI Light"
+font_SU = "Segoe UI"
 class DashboardApp(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -43,7 +44,7 @@ class DashboardApp(ctk.CTk):
 
     def configure_layout(self):
         self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=4)
+        self.grid_columnconfigure(1, weight=9)
         self.grid_columnconfigure(2, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
@@ -52,7 +53,7 @@ class DashboardApp(ctk.CTk):
 
         navbar.grid(row=0, column=0, sticky="nsew")
 
-        titulo = ctk.CTkLabel(navbar, text="MyTask", font=("Arial", 20, "bold"), text_color="#ffffff")
+        titulo = ctk.CTkLabel(navbar, text="MyTask", font=(font_SU, 20, "bold"), text_color="#ffffff")
         titulo.pack(pady=(20, 30))
 
         btn_calendario = ctk.CTkButton(navbar, text="📅 Calendario", anchor="w", fg_color="#0072CE", hover_color="#47ACF1", text_color="white")
@@ -70,7 +71,7 @@ class DashboardApp(ctk.CTk):
         self.center_frame = ctk.CTkFrame(self, corner_radius=10, fg_color="#FFFFFF")
         self.center_frame.grid(row=0, column=1, sticky="nsew", padx=10, pady=20)
 
-        titulo = ctk.CTkLabel(self.center_frame, text="Calendario de Reportes", font=("Segoe UI Light", 18, "normal"), text_color="#003865")
+        titulo = ctk.CTkLabel(self.center_frame, text="Calendario de Reportes", font=(font_SUL, 18, "normal"), text_color="#003865")
         titulo.pack(pady=10)
 
         self.cal = Calendar(self.center_frame, selectmode="day", date_pattern="yyyy-mm-dd")
@@ -83,7 +84,7 @@ class DashboardApp(ctk.CTk):
         self.right_frame = ctk.CTkFrame(self, corner_radius=10, fg_color="#FFFFFF")
         self.right_frame.grid(row=0, column=2, sticky="nsew", padx=10, pady=20)
 
-        self.detalle_titulo = ctk.CTkLabel(self.right_frame, text="Detalles de la Semana", font=("Segoe UI Light", 18, "normal"), text_color="#003865")
+        self.detalle_titulo = ctk.CTkLabel(self.right_frame, text="Detalles de la Semana", font=(font_SUL, 18, "normal"), text_color="#003865")
         self.detalle_titulo.pack(pady=15)
 
         self.cards_frame = ctk.CTkScrollableFrame(self.right_frame, width=300, height=500, fg_color="#FFFFFF")
@@ -100,19 +101,23 @@ class DashboardApp(ctk.CTk):
         for i in range(7):
             fecha = monday + timedelta(days=i)
             clave = fecha.strftime("%Y-%m-%d")
-            if clave in self.reportes:
-                rep = self.reportes[clave]
-                nombre_dia = dias[i]
-                tareas[nombre_dia][rep["hora"]] = {
-                    "titulo": rep["titulo"],
-                    "estado": rep["estado"]
-                }
+            nombre_dia = dias[i]
+
+            for reporte_id, rep in self.reportes.items():
+                if rep["fecha"] == clave:
+                    if rep["hora"] not in tareas[nombre_dia]:
+                        tareas[nombre_dia][rep["hora"]] = []
+                    tareas[nombre_dia][rep["hora"]].append({
+                        "id": reporte_id,  # útil para acciones futuras
+                        "titulo": rep["titulo"],
+                        "estado": rep["estado"]
+                    })
 
         if self.agenda_frame:
             self.agenda_frame.destroy()
 
         self.agenda_frame = AgendaSemanal(self.center_frame, tareas)
-        self.agenda_frame.pack(fill="both", expand=True, pady=10, padx=20)
+        self.agenda_frame.pack(fill="both", expand=True)
 
         self.actualizar_detalles_semana(monday)
 
@@ -150,20 +155,20 @@ class DashboardApp(ctk.CTk):
                 )
                 card.place(in_=sombra, relx=0, rely=0, x=-8, y=-8, relwidth=1, relheight=1) 
 
-                lbl_fecha = ctk.CTkLabel(card, text=fecha.strftime("%A %d/%m"), font=("Arial", 12, "italic"), text_color="#003865")
+                lbl_fecha = ctk.CTkLabel(card, text=fecha.strftime("%A %d/%m"), font=(font_SUL, 12, "italic"), text_color="#003865")
                 lbl_fecha.pack(anchor="w", padx=10, pady=(5, 0))
 
-                lbl_titulo = ctk.CTkLabel(card, text=rep["titulo"], font=("Arial", 14, "bold"), text_color="#003865")
+                lbl_titulo = ctk.CTkLabel(card, text=rep["titulo"], font=(font_SU, 14, "bold"), text_color="#003865")
                 lbl_titulo.pack(anchor="w", padx=10)
 
-                lbl_hora = ctk.CTkLabel(card, text=rep["hora"], font=("Arial", 12), text_color="#003865")
+                lbl_hora = ctk.CTkLabel(card, text=rep["hora"], font=(font_SUL, 12), text_color="#003865")
                 lbl_hora.pack(anchor="w", padx=10)
 
                 estado_color = status_colors.get(rep["estado"], "#817CA5")
                 lbl_estado = ctk.CTkLabel(
                     card,
                     text=f"Estado: {rep['estado']}",
-                    font=("Arial", 12),
+                    font=(font_SUL, 12),
                     text_color=estado_color
                 )
                 lbl_estado.pack(anchor="w", padx=10, pady=(0, 5))
@@ -189,27 +194,36 @@ class AgendaSemanal(ctk.CTkFrame):
         self.crear_grilla()
 
     def crear_grilla(self):
-        ctk.CTkLabel(self.center_frame, text="", width=10).grid(row=0, column=0)
+        ctk.CTkLabel(self.center_frame, text="", width=5).grid(row=0, column=0) 
         for col, dia in enumerate(self.dias, start=1):
-            ctk.CTkLabel(self.center_frame, text=dia, font=("Segoe UI ", 12), width=15, text_color="#003865").grid(row=0, column=col, padx=2, pady=2)
+            ctk.CTkLabel(self.center_frame, text=dia, font=(font_SU, 12), width=15, text_color="#003865").grid(row=0, column=col, padx=2, pady=2)
 
         for row, hora in enumerate(self.horas, start=1):
-            ctk.CTkLabel(self.center_frame, text=hora, font=("Segoe UI Light", 16), width=10, text_color="#003865").grid(row=row, column=0, padx=2, pady=2)
+            ctk.CTkLabel(self.center_frame, text=hora, font=(font_SU, 12), width=12, text_color="#003865").grid(row=row, column=0, padx=2, pady=2)
             for col, dia in enumerate(self.dias, start=1):
-                celda = ctk.CTkFrame(self.center_frame, width=100, height=40, border_width=1, border_color="#003865")
-                celda.grid(row=row, column=col, padx=5, pady=5)
-                celda.grid_propagate(False)
+                celda = ctk.CTkFrame(self.center_frame, width=200, height=80, border_width=1, border_color="#003865", fg_color='transparent')
+                celda.grid(row=row, column=col)
+                celda.grid_propagate(True)
 
-                tarea = self.tareas.get(dia, {}).get(hora)
-                if tarea:
+                tareas_celda = self.tareas.get(dia, {}).get(hora, [])
+                for tarea in tareas_celda:
                     color = status_colors.get(tarea["estado"], "#817CA5")
+                    frame_con_borde = ctk.CTkFrame(
+                        master=celda,
+                        fg_color="#003865",
+                        corner_radius=5
+                    )
+                    frame_con_borde.pack(fill="x", padx=2, pady=2)  # uno bajo otro
+                    
                     ctk.CTkLabel(
-                            celda,
-                            text=tarea["titulo"],
-                            text_color="white",
-                            fg_color=color,
-                            corner_radius=5
-                        ).pack(expand=True, fill="both", padx=2, pady=2)
+                        master=frame_con_borde,
+                        text=tarea["titulo"],
+                        text_color="white",
+                        fg_color=color,
+                        corner_radius=5
+                    ).pack(fill="x", padx=2, pady=1)
+
+                    
 
 
 if __name__ == "__main__":
